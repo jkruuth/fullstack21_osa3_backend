fc0268c68bf388d9f38ebef27f34cb94ef786a24
jkruuth <joonas.k.ruuth@student.jyu.fi>
2021-06-01 16:35:26 +0300

    lisa

--- index.js
+++ index.js
@@ -108,6 +108,12 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+  
+  app.use(unknownEndpoint)
+
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
