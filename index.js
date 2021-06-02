06efdc1408f0146bb47fc63abf1a24a090370a54
jkruuth <joonas.k.ruuth@student.jyu.fi>
2021-06-02 20:11:33 +0300

    3.15

--- index.js
+++ index.js
@@ -6,10 +6,9 @@ const Person = require('./models/person')
 
 const app = express()
 
+app.use(express.static('build'))
 app.use(express.json())
 app.use(cors())
-app.use(express.static('build'))
-
 
 /* let persons = [
     {
@@ -97,11 +96,32 @@ app.post('/api/persons', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
+    Person.findByIdAndRemove(request.params.id).then(result => {
+        response.status(204).end()
+    })
+    .catch(error => next(error))
 })
+
+const unknownEndpoint = (request, response) => {
+    response.status.send({ error: 'Unknown endpoint' })
+  }
+
+  // olemattomien osoitteiden käsittely
+  app.use(unknownEndpoint)
+
+
+  // virheellisten pyyntöjen käsittely
+  const errorHandler = (error, request, response, next) => {
+    console.log(error.message)
+
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id '})
+    }
+
+    next(error)
+  }
+
+  app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
